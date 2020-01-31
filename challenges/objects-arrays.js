3c6023fe533f2c491be94271d103a0b74670e864
Wayne <wabbruscatoorr@gmail.com>
2020-01-31 11:43:40 -0500

    updating answers

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,18 +7,38 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const tyrannosaurus = {
+name: "tyrannosaurus",
+diet: "carnivorous",
+weight: 7000,
+length: 12,
+period: "Late Cretaceous"
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const stegosaurus = {
+  name: "stegosaurus",
+  diet: "herbivorous",
+  weight: 2000,
+  length: 9,
+  period: "Late Jurassic"
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+const velociraptor = {
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight: 15, 
+length: 1.8,
+period: "Late Cretaceous"
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.);
 
 // How long was a stegosaurus?
 console.log();
